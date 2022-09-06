9578e6aa786e4d3cf789de2fe2afdd1a2660876f
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2022-09-06 18:31:00 -0500

    Agregar control para aumentar/reducir velocidad de animación

--- gameoflife/include/config.hpp
+++ gameoflife/include/config.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+namespace Config {
 const int CELL_SIZE = 10;
 const int GRID_WIDTH = 100;
 const int GRID_HEIGHT = 100;
@@ -11,5 +12,10 @@ const int ZOOM_FACTOR = 2;
 const int ZOOM_DEFAULT = 1;
 const int MARGIN = 10;
 const int SCROLL_AMOUNT = CELL_SIZE;
-const bool SHOULD_DRAW_GRID_LINES = true;
+const bool SHOULD_DRAW_GRID_LINES = false;
 const bool DEBUG = false;
+const bool SHOULD_MODULE_INDEXES = true;
+const double SPEED_FACTOR = 10;
+const double MIN_UPDATE_RATE_MS = 10;
+const double MAX_UPDATE_RATE_MS = 5000;
+};  // namespace Config
