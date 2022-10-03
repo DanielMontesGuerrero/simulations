fd7b7cce3137b657231904c9ded684ea1c2930cb
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2022-10-03 17:30:08 -0500

    Configurar package version

--- azure/simulationsApp/start_orchestrator/config.py
+++ azure/simulationsApp/start_orchestrator/config.py
@@ -11,10 +11,11 @@ POOL_ID = os.getenv('POOL_ID', '')
 JOB_ID = os.getenv('JOB_ID', '')
 INBOUND_ENDPOINT_NAME = os.getenv('INBOUND_ENDPOINT_NAME', '')
 BACK_END_PORT = int(os.getenv('BACK_END_PORT', '3000'))
+APP_PACKAGE_VERSION = os.getenv('APP_PACKAGE_VERSION', '').replace('.', '_')
 
-WORKER_COMMAND = ('/bin/bash -c \"cd $AZ_BATCH_APP_PACKAGE_orchestrator_0_0_2 &&'
-                  ' ./worker -rows={} -cols={} -host={} -port={} -protocol={}\"')
+WORKER_COMMAND = ('/bin/bash -c \"cd $AZ_BATCH_APP_PACKAGE_orchestrator_' + APP_PACKAGE_VERSION
+                  + ' && ./worker -rows={} -cols={} -host={} -port={} -protocol={}\"')
 
-ORCH_COMMAND = ('/bin/bash -c \"cd $AZ_BATCH_APP_PACKAGE_orchestrator_0_0_2 &&'
-                ' ./orchestrator -rows={} -cols={} -host={} -port={} -protocol={} '
+ORCH_COMMAND = ('/bin/bash -c \"cd $AZ_BATCH_APP_PACKAGE_orchestrator_' + APP_PACKAGE_VERSION
+                + ' && ./orchestrator -rows={} -cols={} -host={} -port={} -protocol={} '
                 '-hosts={} -ports={}\"')
